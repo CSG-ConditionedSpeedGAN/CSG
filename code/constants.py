322cdba0b85ef2bd8516d3317e8c656add4e1167
Vishal Sowrirajan <vishalsowrirajan5@gmail.com>
2021-03-23 10:47:40 +0100

    Update README.MD

--- code/constants.py
+++ code/constants.py
@@ -5,7 +5,7 @@ MULTI_TRAIN_DATASET_PATH = ''
 MULTI_VAL_DATASET_PATH = ''
 MULTI_TEST_DATASET_PATH = ''
 
-CHECKPOINT_NAME = ''
+CHECKPOINT_NAME = 'Checkpoints/No_Agg/eth.pt'
 
 SINGLE_TRAIN_DATASET_PATH = 'single_condition_dataset/eth/train'
 SINGLE_VAL_DATASET_PATH = 'single_condition_dataset/eth/val'
@@ -62,7 +62,7 @@ L2_LOSS_WEIGHT = 1
 
 NUM_ITERATIONS = 3200
 AGGREGATION_TYPE = 'None'  # the aggregations can be pooling or concat or attention or None
-USE_GPU = 0  # use 1 for ETH/UCY and 0 for Argoverse
+USE_GPU = 1  # use 1 for ETH/UCY and 0 for Argoverse
 
 # SPEED CONTROL FLAGS
 TEST_METRIC = 2  # To simulate trajectories, change the flag to 2 and for prediction environment, change the flag to 1.
